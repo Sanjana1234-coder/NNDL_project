986fd58b4ae9949d975bf2c20abadb125cb480b5
sanjana budankayala <121283885+Sanjana1234-coder@users.noreply.github.com>
2024-02-10 11:48:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import os
+from realtime import starter;
+from picture import imz;
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if 'image' in request.files:
+        image = request.files['image']
+        img_name = image.filename
+        image.save(os.path.join(UPLOAD_FOLDER, img_name))
+        imz(img_name);
+        return "Image uploaded successfully!"
+    else:
+        return "No image file found."
+    
+@app.route('/video')
+def video():
+    starter();
+
+if __name__ == '__main__': 
+    app.run(debug=True)
